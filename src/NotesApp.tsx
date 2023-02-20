18c066f0cc49c708d35b0c89affd0a4f6a8c028b
Lucas <lucassanchezdl@gmail.com>
2023-02-20 11:15:36 -0300

    first commit

---
+++ src/NotesApp.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function NotesApp() {
+  return <h1>Vite + React +Typescript</h1>;
+}
+
+export default NotesApp;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotesApp from "./NotesApp";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <NotesApp />
+  </React.StrictMode>
+);
